ac4d252495fcadac0c25202333ac988ce8553dc5
DiamondMC <89172623+DiamondMC@users.noreply.github.com>
2022-06-29 09:41:09 -0400

    Add files via upload

---
+++ F7/src/main/java/me/diamondmc/f7/F7.java
@@ -0,0 +1,15 @@
+package me.diamondmc.f7;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class F7 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
